5d4fc67d5943f0e5f2cb4a1a3c2a6256c6b0cddd
Student <example@example.com>
2019-11-20 19:50:13 -0800

    add names and new logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,17 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
 
 $(document).ready(function() {
-  $("form#calculator").submit(function() {
+  $("form#celebs").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
+    var name1 = ($("#input1").val());
+    var name2 = ($("#input2").val());
     var operator = $("input:radio[name=operator]:checked").val();
-    console.log("1st number: " + number1);  // for debugging
-    console.log("2nd number: " + number2);  // for debugging
+    console.log("1st name: " + name1);  // for debugging
+    console.log("2nd name: " + name2);  // for debugging
     console.log("operator: " + operator);  // for debugging
-    var result = add(number1, number2);
+    var result = add(name1, name2);
     $("#output").text(result);
   });
 });
